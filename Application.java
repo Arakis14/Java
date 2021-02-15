cc30116601a8210a19294cc22e27ee34587c8c7a
Wiktor <wiktor.lasota@vp.pl>
2021-02-15 09:31:32 +0100

    Strings

---
+++ Application.java
@@ -0,0 +1,18 @@
+
+public class Application {
+    public static void main (String[] args ) {
+        int myInt = 13;
+
+        String text = "Hi there!";
+
+        String name = "Mike";
+
+        String blank = " ";
+
+        System.out.println(text + "aaa");
+
+        System.out.println(text + blank + name);
+
+        System.out.println("My int is: " + myInt);
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ my udemy Java course
 Topics:
 1. Hello World
 2. Variables
+3. Strings
