c29a81308f3c38c5fdfbba284739f06e474fe992
Wiktor <wiktor.lasota@vp.pl>
2021-04-16 14:08:35 +0200

    Classes

---
+++ Classy.java
@@ -0,0 +1,33 @@
+import java.util.function.Function;
+
+class Animal {
+
+    boolean human;
+    int legs;
+    String name;
+
+    Function printer () {
+        System.out.println(this.name + " " + this.legs + " " + this.human);
+        return null;
+    }
+}
+
+
+public class Classy {
+
+    public static void main(String[] args) {
+
+        Animal cat = new Animal();
+        cat.human = false;
+        cat.legs = 4;
+        cat.name = "Xurek";
+
+        Animal man = new Animal();
+        man.human = true;
+        man.legs = 2;
+        man.name = "Mark";
+
+        man.printer();
+        cat.printer();
+    }
+}
